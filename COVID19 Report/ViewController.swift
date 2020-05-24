a34c980240ecafa3ad6c65b586ee15fa316ee6b8
Siavash <svshjalali@gmail.com>
2020-05-24 20:43:43 +0900

    Initial Commit

---
+++ COVID19 Report/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  COVID19 Report
+//
+//  Created by Siavash Jalali on 2020/05/24.
+//  Copyright © 2020 Shichan Official. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
